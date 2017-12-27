c4c1272a3763cb904697ec31e237e0ce40cc83a3
Abhi Ramani <abhiramani3@gmail.com>
2017-12-27 19:11:26 +0530

    add email and username validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 15}
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 15}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
