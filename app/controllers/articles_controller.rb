af527b116f0426ac2465073e0278f0ddd2838f97
Abhi Ramani <abhiramani3@gmail.com>
2017-12-27 22:21:36 +0530

    add association between user and article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,8 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(article_params)       
+        @article = Article.new(article_params)
+        @article.user = User.first       
         if @article.save       
             flash[:notice] = "Article was successfully created"        
             redirect_to article_path(@article)        
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+    has_many :articles
+    before_save { self.email = email.downcase }
+
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 15}
     validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 15}, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
